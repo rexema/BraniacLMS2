0e2ae8ef253bc5dbc6a88fc20829499d042fc838
rexema <anast82@mail.ru>
2022-06-11 18:10:53 +0500

    lesson_2 Registration

--- config/settings.py
+++ config/settings.py
@@ -66,7 +66,6 @@ TEMPLATES = [
                 "django.template.context_processors.media",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
-                "mainapp.context_processors.example.simple_context_processor",
             ],
         },
     },
