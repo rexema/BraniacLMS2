7582c59c8df254ddbf997020cf6a51d9adf1fb5a
rexema <anast82@mail.ru>
2022-06-11 18:48:56 +0500

    lesson_6 Link to admin

--- config/settings.py
+++ config/settings.py
@@ -67,7 +67,6 @@ TEMPLATES = [
                 "django.template.context_processors.media",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
-                "mainapp.context_processors.example.simple_context_processor",
                 "social_django.context_processors.backends",
                 "social_django.context_processors.login_redirect",
             ],
