fab7e9d6a212fc5d9e057ffc4f3f86d258ce1c19
rexema <anast82@mail.ru>
2022-06-16 22:11:02 +0500

    lesson_7 Email via celery

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,15 +1,20 @@
 import logging
 
 from django.conf import settings
+from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin, UserPassesTestMixin
+from django.core.cache import cache
 from django.http import FileResponse, JsonResponse
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import get_object_or_404
 from django.template.loader import render_to_string
 from django.urls import reverse_lazy
+from django.utils.translation import gettext_lazy as _
 from django.views.generic import CreateView, DeleteView, DetailView, ListView, TemplateView, UpdateView, View
 
 from mainapp import forms as mainapp_forms
 from mainapp import models as mainapp_models
+from mainapp import tasks as mainapp_tasks
 
 logger = logging.getLogger(__name__)
 
@@ -63,7 +68,7 @@ class CoursesDetailView(TemplateView):
     template_name = "mainapp/courses_detail.html"
 
     def get_context_data(self, pk=None, **kwargs):
-        logger.debug(f"Yet another log messag: title = {pk}")
+        logger.debug("Yet another log message")
         context = super(CoursesDetailView, self).get_context_data(**kwargs)
         context["course_object"] = get_object_or_404(mainapp_models.Courses, pk=pk)
         context["lessons"] = mainapp_models.Lesson.objects.filter(course=context["course_object"])
@@ -75,9 +80,18 @@ class CoursesDetailView(TemplateView):
                 context["feedback_form"] = mainapp_forms.CourseFeedbackForm(
                     course=context["course_object"], user=self.request.user
                 )
-        context["feedback_list"] = mainapp_models.CourseFeedback.objects.filter(
-            course=context["course_object"]
-        ).order_by("-created", "-rating")[:5]
+
+        cached_feedback = cache.get(f"feedback_list_{pk}")
+        if not cached_feedback:
+            context["feedback_list"] = (
+                mainapp_models.CourseFeedback.objects.filter(course=context["course_object"])
+                .order_by("-created", "-rating")[:5]
+                .select_related()
+            )
+            cache.set(f"feedback_list_{pk}", context["feedback_list"], timeout=300)  # 5 minutes
+        else:
+            context["feedback_list"] = cached_feedback
+
         return context
 
 
@@ -93,6 +107,36 @@ class CourseFeedbackFormProcessView(LoginRequiredMixin, CreateView):
 
 class ContactsPageView(TemplateView):
     template_name = "mainapp/contacts.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(ContactsPageView, self).get_context_data(**kwargs)
+        if self.request.user.is_authenticated:
+            context["form"] = mainapp_forms.MailFeedbackForm(user=self.request.user)
+        return context
+
+    def post(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            cache_lock_flag = cache.get(f"mail_feedback_lock_{self.request.user.pk}")
+            if not cache_lock_flag:
+                cache.set(
+                    f"mail_feedback_lock_{self.request.user.pk}",
+                    "lock",
+                    timeout=300,
+                )
+                messages.add_message(self.request, messages.INFO, _("Message sended"))
+                mainapp_tasks.send_feedback_mail.delay(
+                    {
+                        "user_id": self.request.POST.get("user_id"),
+                        "message": self.request.POST.get("message"),
+                    }
+                )
+            else:
+                messages.add_message(
+                    self.request,
+                    messages.WARNING,
+                    _("You can send only one message per 5 minutes"),
+                )
+        return HttpResponseRedirect(reverse_lazy("mainapp:contacts"))
 
 
 class DocSitePageView(TemplateView):
