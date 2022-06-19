e2bcd07577ac7f29cc7f635bfb831d58bedf1bd0
rexema <anast82@mail.ru>
2022-06-19 16:05:22 +0500

    lesson_8 i18n

--- config/settings.py
+++ config/settings.py
@@ -50,10 +50,10 @@ INSTALLED_APPS = [
     "crispy_forms",
     "debug_toolbar",
 ]
-
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.locale.LocaleMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -211,3 +211,4 @@ CELERY_RESULT_BACKEND = "redis://localhost:6379"
 # Email as files for debug
 EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
 EMAIL_FILE_PATH = "var/email-messages/"
+LOCALE_PATHS = [BASE_DIR / "locale"]
